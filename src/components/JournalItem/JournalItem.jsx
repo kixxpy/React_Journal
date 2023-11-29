da6d118e251af7799b87ab755d2177403df12c67
kirillEN <81442482+kixxpy@users.noreply.github.com>
2023-11-29 23:18:05 +0300

    add splice post

--- src/components/JournalItem/JournalItem.jsx
+++ src/components/JournalItem/JournalItem.jsx
@@ -2,9 +2,10 @@ import './JournalItem.css';
 
 function JournalItem({ title, post, date }) {
 	const formatedDate = new Intl.DateTimeFormat('ru-RU').format(date);
-	let sliced = post.slice(0, 30);
-	if (sliced.length < post.length) {
-		sliced += '...';
+
+	let text = post.slice(0, 30);
+	if (text.length < post.length) {
+		text += '...';
 	}
 
 	return (
@@ -12,7 +13,7 @@ function JournalItem({ title, post, date }) {
 			<h2 className="journal-item__header">{title}</h2>
 			<h2 className="journal-item__body">
 				<div className="journal-item__date">{formatedDate}</div>
-				<div className="journal-item__text">{sliced}</div>
+				<div className="journal-item__text">{text}</div>
 			</h2>
 		</>
 	);
