e5e9cb325bd94dd9d429dc70be69ebb17368e517
kirillEN <81442482+kixxpy@users.noreply.github.com>
2023-11-29 23:04:03 +0300

    Добавлена функциональность обрезание до 30 символов приходящего поста

--- src/components/JournalItem/JournalItem.jsx
+++ src/components/JournalItem/JournalItem.jsx
@@ -2,13 +2,17 @@ import './JournalItem.css';
 
 function JournalItem({ title, post, date }) {
 	const formatedDate = new Intl.DateTimeFormat('ru-RU').format(date);
+	let sliced = post.slice(0, 30);
+	if (sliced.length < post.length) {
+		sliced += '...';
+	}
 
 	return (
 		<>
 			<h2 className="journal-item__header">{title}</h2>
 			<h2 className="journal-item__body">
 				<div className="journal-item__date">{formatedDate}</div>
-				<div className="journal-item__text">{post}</div>
+				<div className="journal-item__text">{sliced}</div>
 			</h2>
 		</>
 	);
--- src/components/SelectUser/SelectUser.jsx
+++ src/components/SelectUser/SelectUser.jsx
@@ -11,8 +11,8 @@ function SelectUser() {
 	
 	return (
 		<select className={styles['select']} name="user" id="user" value={userId} onChange={changeUser}>
-			<option value="1">Антон</option>
-			<option value="2">Вася</option>
+			<option value="1">Личное</option>
+			<option value="2">Рабочее</option>
 		</select>
 	);
 }
